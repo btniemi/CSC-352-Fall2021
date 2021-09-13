126094382b32f544c6952e6293b7310848629827
btniemi <braedon.niemi@rocky.edu>
2021-09-13 16:39:09 -0600

    work in class

--- Assignments/test.py
+++ Assignments/test.py
@@ -1,14 +1,20 @@
-dict = {'a': 1}
-print(dict)
-
-dict.update(a='hello')
-print(dict)
-
-person_dict = {
-    'fName': "Braedon",
-    'lName': "Niemi",
-    'age': 27,
-    'grade': ['A', 'B', 'C', 'A']
-}
+# dict = {'a': 1}
+# print(dict)
+#
+# dict.update(a='hello')
+# print(dict)
+#
+# person_dict = {
+#     'fName': "Braedon",
+#     'lName': "Niemi",
+#     'age': 27,
+#     'grade': ['A', 'B', 'C', 'A']
+# }
 
 # was trying to test changes in grade list but realiezed that easier to create class and each grade for a class should be seperate that you can change not one list
+
+languages = ['Java', 'Python', 'JavaScript']
+versions = [14, 3, 6]
+
+result = zip(languages, versions)
+print(list(result))
