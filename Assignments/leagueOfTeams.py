644aec1531fc82b82c32e65039da4c5a1ce5c8a8
Braedon Niemi <70549370+btniemi@users.noreply.github.com>
2021-10-18 09:33:55 -0600

    project work

--- Assignments/leagueOfTeams.py
+++ Assignments/leagueOfTeams.py
@@ -16,70 +16,88 @@ class League():
     def tie_score(self):
         self.tie += 1
 
-# might not need this function because might be able to do it above in __init__
+    # might not need this function because might be able to do it above in __init__
     def win_lose_percentage(self):
-        self.percentage = self.win / self.lose
+        if self.lose == 0 and self.win > 0:
+            self.percentage = '100%'
+        elif self.lose == 0 and self.win == 0:
+            self.percentage = 'not_enough_data_to_compute'
+        else:
+            self.percentage = self.win / self.lose
 
 
 teams = [League() for x in range(0, 8)]
 
-#loop to do this for 8 teams each round its like a double loop type thing should be easy to figure out
+# loop for adding scores in
+# while True:
 
-pickTeam = input("Which team do you want to change there scores? (ONE,TWO,THREE,FOUR,FIVE,SIX,SEVEN, or EIGHT) ")
+pickTeam = str(input("Which team do you want to change there scores? (type done if finished) "))
+    # if pickTeam == 'done':
+    #     break
 gameResult = input("Did they win, lose, or tie? ")
 
-if pickTeam == "ONE" and gameResult == 'win':
+if pickTeam == "ONE" or '1' or 'one' and gameResult == 'win':
     teams[0].win_score()
-elif pickTeam == "ONE" and gameResult == 'lose':
+elif pickTeam == "ONE" or '1' or 'one' and gameResult == 'lose':
     teams[0].lose_score()
-elif pickTeam == "ONE" and gameResult == 'tie':
+elif pickTeam == "ONE" or '1' or 'one' and gameResult == 'tie':
     teams[0].tie_score()
-elif pickTeam == "TWO" and gameResult == 'win':
+if pickTeam == "TWO" or '2' or 'two' and gameResult == 'win':
     teams[1].win_score()
-elif pickTeam == "TWO" and gameResult == 'lose':
+elif pickTeam == "TWO" or '2' or 'two' and gameResult == 'lose':
     teams[1].lose_score()
-elif pickTeam == "TWO" and gameResult == 'tie':
+elif pickTeam == "TWO" or '2' or 'two' and gameResult == 'tie':
     teams[1].tie_score()
-elif pickTeam == "THREE" and gameResult == 'win':
+elif pickTeam == "THREE" or '3' or 'three' and gameResult == 'win':
     teams[2].win_score()
-elif pickTeam == "THREE" and gameResult == 'lose':
+elif pickTeam == "THREE" or '3' or 'three' and gameResult == 'lose':
     teams[2].lose_score()
-elif pickTeam == "THREE" and gameResult == 'tie':
+elif pickTeam == "THREE" or '3' or 'three' and gameResult == 'tie':
     teams[2].tie_score()
-elif pickTeam == "FOUR" and gameResult == 'win':
+elif pickTeam == "FOUR" or '4' or 'four' and gameResult == 'win':
     teams[3].win_score()
-elif pickTeam == "FOUR" and gameResult == 'lose':
+elif pickTeam == "FOUR" or '4' or 'four' and gameResult == 'lose':
     teams[3].lose_score()
-elif pickTeam == "FOUR" and gameResult == 'tie':
+elif pickTeam == "FOUR" or '4' or 'four' and gameResult == 'tie':
     teams[3].tie_score()
-elif pickTeam == "FIVE" and gameResult == 'win':
+elif pickTeam == "FIVE" or '5' or 'five' and gameResult == 'win':
     teams[4].win_score()
-elif pickTeam == "FIVE" and gameResult == 'lose':
+elif pickTeam == "FIVE" or '5' or 'five' and gameResult == 'lose':
     teams[4].lose_score()
-elif pickTeam == "FIVE" and gameResult == 'tie':
+elif pickTeam == "FIVE" or '5' or 'five' and gameResult == 'tie':
     teams[4].tie_score()
-elif pickTeam == "SIX" and gameResult == 'win':
+elif pickTeam == "SIX" or '6' or 'six' and gameResult == 'win':
     teams[5].win_score()
-elif pickTeam == "SIX" and gameResult == 'lose':
+elif pickTeam == "SIX" or '6' or 'six' and gameResult == 'lose':
     teams[5].lose_score()
-elif pickTeam == "SIX" and gameResult == 'tie':
+elif pickTeam == "SIX" or '6' or 'six' and gameResult == 'tie':
     teams[5].tie_score()
-elif pickTeam == "SEVEN" and gameResult == 'win':
+elif pickTeam == "SEVEN" or '7' or 'seven' and gameResult == 'win':
     teams[6].win_score()
-elif pickTeam == "SEVEN" and gameResult == 'lose':
+elif pickTeam == "SEVEN" or '7' or 'seven' and gameResult == 'lose':
     teams[6].lose_score()
-elif pickTeam == "SEVEN" and gameResult == 'tie':
+elif pickTeam == "SEVEN" or '7' or 'seven' and gameResult == 'tie':
     teams[6].tie_score()
-elif pickTeam == "EIGHT" and gameResult == 'win':
+elif pickTeam == "EIGHT" or '8' or 'eight' and gameResult == 'win':
     teams[7].win_score()
-elif pickTeam == "EIGHT" and gameResult == 'lose':
+elif pickTeam == "EIGHT" or '8' or 'eight' and gameResult == 'lose':
     teams[7].lose_score()
-elif pickTeam == "EIGHT" and gameResult == 'tie':
+elif pickTeam == "EIGHT" or '8' or 'eight' and gameResult == 'tie':
     teams[7].tie_score()
 
-# HOW TO DO WIN LOSE PERCENTAGE in loop here to get them to print out
+# calc win percentage
+
+teams[0].win_lose_percentage()
+teams[1].win_lose_percentage()
+teams[2].win_lose_percentage()
+teams[3].win_lose_percentage()
+teams[4].win_lose_percentage()
+teams[5].win_lose_percentage()
+teams[6].win_lose_percentage()
+teams[7].win_lose_percentage()
 
 count = 1
 for team in teams:
-    print("Team", count, ": Wins", team.win, "Loses", team.lose, "Ties", team.tie, "Win/Lose Percentage", team.percentage)
+    print("Team", count, ": Wins", team.win, "Loses", team.lose, "Ties", team.tie, "Win/Lose Percentage",
+          team.percentage)
     count += 1
